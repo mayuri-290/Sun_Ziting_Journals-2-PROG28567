844cb7623e7c0cf5c3b2122d9a57ee4fb2c0f98a
mayuri-290 <sunziting@hotmail.com>
2025-11-22 18:09:56 -0500

    fixed some problems in task 3's script.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,12 +14,12 @@ public class PlayerController : MonoBehaviour
     public float apexTime, apexHeight;
     public float gravity, initialJumpVelocity;
     private bool jumpTrigger;
+    private bool jumpTest;
     public float terminalSpeed=-20f;
     public float coyoteTime;
     private float coyoteTimer=0f;
 
 
-    bool isWalking;
     bool isGrounded;
 
     public enum FacingDirection
@@ -41,26 +41,27 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector2 pos = new Vector2(transform.position.x, transform.position.y - 0.51f);
+        bool wasGrounded = isGrounded;
+        
+        bool hitGround = Physics2D.Raycast(pos, Vector2.down, 0.2f);
+        isGrounded = hitGround;
+        Debug.DrawRay(pos,Vector2.down * 0.2f, Color.red);
+
+          if(isGrounded&& !wasGrounded)
+        {
+            jumpTest = false;
+            coyoteTimer = coyoteTime;
+        }
+        else if(!isGrounded)
+        {
+            coyoteTimer -= Time.deltaTime;
+        }
         //The input from the player needs to be determined and then passed in the to the MovementUpdate which should
         //manage the actual movement of the character.
         playerInput = new Vector2();
         MovementUpdate(playerInput);
-
-        //Debug.Log(playerInput);
-
-        bool hitGround = Physics2D.Raycast(transform.position, Vector2.down, 0.1f);
-        isGrounded = hitGround;
-        Debug.DrawRay(transform.position, Vector2.down * 0.1f, Color.red);
-
-        if(isGrounded)
-        {
-            coyoteTimer = coyoteTime;
-        }
-        else
-        {
-            coyoteTimer -= Time.deltaTime;
-        }
-        
+        //Debug.Log(playerInput);     
     }
 
     private void FixedUpdate()
@@ -81,12 +82,12 @@ public class PlayerController : MonoBehaviour
     private void MovementUpdate(Vector2 playerInput)
     {
         playerInput.x = Input.GetAxisRaw("Horizontal");
-
         playerRigidbody.AddForce(playerInput * speed);     
 
-        if(Input.GetKeyDown(KeyCode.Space) && (isGrounded ||coyoteTimer>0f))
-        {
+    if (Input.GetKeyDown(KeyCode.Space) && !jumpTest && (isGrounded || coyoteTimer > 0f))        
+{
             jumpTrigger = true;
+            jumpTest = true;
         }
     }
 
@@ -110,7 +111,6 @@ public class PlayerController : MonoBehaviour
         {
             currentFacing = FacingDirection.left;
         }
-
         return currentFacing;
     }
 }
