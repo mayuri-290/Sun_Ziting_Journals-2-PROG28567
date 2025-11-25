aaed02aa0bbb25f9f2c21a177eb1083584d36b97
mayuri-290 <sunziting@hotmail.com>
2025-11-25 10:30:24 -0500

    Add class code and updated character sprite sheet.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -18,6 +18,10 @@ public class PlayerController : MonoBehaviour
     public float terminalSpeed=-20f;
     public float coyoteTime;
     private float coyoteTimer=0f;
+    public float maxSpeed;
+    public float acceleration;
+    public float deceleration;
+    private Vector3 currentVelocity;
 
 
     bool isGrounded;
@@ -85,11 +89,34 @@ public class PlayerController : MonoBehaviour
         playerRigidbody.AddForce(playerInput * speed);     
 
     if (Input.GetKeyDown(KeyCode.Space) && !jumpTest && (isGrounded || coyoteTimer > 0f))        
-{
+        {
             jumpTrigger = true;
             jumpTest = true;
         }
-    }
+//------------------Class code------------------//
+        // if(playerInput.x != 0)
+        // {
+        //    currentVelocity += playerInput.x * acceleration * Vector3.right * Time.deltaTime;
+        //    if(Mathf.Abs(currentVelocity.x) > maxSpeed)
+        //    {
+        //     currentVelocity = new Vector3(Mathf.Sign(currentVelocity.x) * maxSpeed, currentVelocity.y);
+        //    }
+        // }
+        // else
+        // {
+        //     Vector3 amountWeWantToChange = deceleration * currentVelocity.normalized * Time.deltaTime;
+
+        //     if(amountWeWantToChange.magnitude > currentVelocity.x)
+        //     {
+        //         currentVelocity.x = 0;
+        //     }
+        //     else
+        //     {
+        //         currentVelocity -= amountWeWantToChange;
+        //     }
+        // }
+//------------------Class code------------------//
+     }
 
     public bool IsWalking()
     {
